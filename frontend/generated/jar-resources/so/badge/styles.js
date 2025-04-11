5a451709cc7e67069946dcc159d1a00002b95df2
Syam Pillai <syam.s.pillai@gmail.com>
2025-04-11 12:10:22 +0400

    Version 2.6 SO Components Version 13.0.0

--- frontend/generated/jar-resources/so/badge/styles.js
+++
@@ -1,9 +0,0 @@
-import '@vaadin/vaadin-lumo-styles/badge.js';
-
-const $_documentContainer = document.createElement('template');
-$_documentContainer.innerHTML = `
-    <custom-style>
-        <style include="lumo-badge"></style>
-    </custom-style>`;
-
-document.head.appendChild($_documentContainer.content);
